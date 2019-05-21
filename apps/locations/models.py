4e9aa897b2425b6671cc7399396749256ff7b5c2
Weston Harper <wes@tao.team>
2019-05-21 16:26:20 -0600

    added initial migration and schema for locations

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class LocationManager(models.Manager):
+    pass
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    min_gold = models.IntegerField()
+    max_gold = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
