14544317957f2a1d08a700e3ed4e6c1717ef2df3
Weston Harper <wes@tao.team>
2019-05-22 17:02:45 -0600

    changed tasks url to locations/

--- ninja_gold/urls.py
+++ ninja_gold/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('apps.locations.urls')),
+    path('locations/', include('apps.locations.urls')),
 ]
