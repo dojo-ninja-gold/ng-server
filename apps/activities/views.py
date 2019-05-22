889fe0490508f9aa543aa103276e56bfef62cf40
Weston Harper <wes@tao.team>
2019-05-22 17:17:40 -0600

    added routes for acitvities index

--- apps/activities/views.py
+++ apps/activities/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from .models import Activity
+from django.core import serializers
 
 # Create your views here.
+def index(req, user_id):
+    activities = Activity.objects.filter(user=user_id)
+    json_activities = serializers.serialize('json', activities)
+    return HttpResponse(json_activities, status=200, content_type='application/json')
+
+# def index_with_params(req):
+#     print(req.GET)
+#     return HttpResponse('params')
