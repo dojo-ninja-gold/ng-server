86c0c13385781414eefb3930dc2751ec6ef94dcf
Weston Harper <wes@tao.team>
2019-05-23 16:23:29 -0600

    added validator to UserManager and attached user manager to objects

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your models here.
 class UserManager(models.Manager):
-    pass
+    def validate(self, data):
+        errors = []
+        if len(data['first_name']) < 2:
+            errors.append('First name must be at least 2 characters long')
+        if len(data['last_name']) < 2:
+            errors.append('Last name must be at least 2 characters long')
+        if not EMAIL_REGEX.match(data['email']):
+            errors.append('Email must be valid')
+        if len(data['password']) < 8:
+            errors.append('Password must be at least 2 characters long')
+        return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
@@ -11,3 +23,4 @@ class User(models.Model):
     pw_hash = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
