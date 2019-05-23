651c3999cfa2fe4d283c2ed7c24f19b43590b041
Weston Harper <wes@tao.team>
2019-05-23 16:19:46 -0600

    add show route and skeleton for users api views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+from .models import User
+from django.core import serializers
 # Create your views here.
+def show(req, user_id):
+    user = User.objects.filter(id=user_id)
+    json_user = serializers.serialize('json', user)
+    return HttpResponse(json_user, status=200, content_type='application/json')
+
+def create(req):
+    pass
+
+def login(req):
+    pass
