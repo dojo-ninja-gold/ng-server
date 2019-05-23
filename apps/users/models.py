745e19df17f820f1c696c76aafed7c72b3448f67
Weston Harper <wes@tao.team>
2019-05-23 16:18:47 -0600

    updated user models for login reg

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,6 +5,9 @@ class UserManager(models.Manager):
     pass
 
 class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    pw_hash = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
